8e9180b9197c4bc23a4c6ec927bd9104f8b02b1d
vulopez <111790061+vulopez@users.noreply.github.com>
2023-09-19 20:42:35 -0600

    Ruta y Controlador para testing de componentes
    Se agregan la ruta y el controlador por defecto para hacer testing de los componentes puros

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/static/(:segment)', 'Component::static/$1');
--- app/Controllers/Component.php
+++ app/Controllers/Component.php
@@ -4,5 +4,10 @@ namespace App\Controllers;
 
 class Component extends BaseController
 {
-    
+    public function static($component)
+    {
+        echo view('templates/header', ['title' => $component]);
+        echo view('components/' . $component, ['group_grade' => 'Básicos']);
+        echo view('templates/footer');
+    }
 }
