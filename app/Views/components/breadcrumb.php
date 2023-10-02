0ec912ff32dadaf717a6d71ea9242dbf7a096ce2
bm18if <braylin.garcia.202109020@gmail.com>
2023-10-01 22:22:13 -0600

    inicio breadcrumb

---
+++ app/Views/components/breadcrumb.php
@@ -0,0 +1,7 @@
+<ul class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="">Inicio </a></li>
+          <li class="breadcrumb-item">
+            <a href="Perfiles">Perfiles</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Encargados</li>
+        </ul>
